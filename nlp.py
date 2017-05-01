49530a039f40185d09eb6af0b2e7061e2f1ebc13
rupanroy6 <rupan.roy6@gmail.com>
2017-05-01 22:00:30 +0530

    modified nlp.py

--- nlp.py
+++ nlp.py
@@ -12,7 +12,7 @@ auth.set_access_token(access_token,access_token_secret)
 
 api=tweepy.API(auth)
 
-public_tweets = api.search('rupan roy')
+public_tweets = api.search('trump') #replace with what you wart to search
 
 for tweet in public_tweets:
     print(tweet.text)
